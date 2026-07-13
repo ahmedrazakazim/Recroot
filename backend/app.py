24171039ed493218ec11f61079a5ff62c8b31ccd
simal24k <simal.mpgss@gmail.com>
2026-07-13 15:19:37 +0500

    Fix CORS for Vercel

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
-    CORS(app)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
     db.init_app(app)
     JWTManager(app)
     
