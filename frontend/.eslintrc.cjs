9630bf52de56351af2f68fa46494d14b124754ac
simal24k <simal.mpgss@gmail.com>
2026-07-13 15:41:30 +0500

    Downgrade ESLint errors to warnings

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,1 @@
+module.exports = { root: true, env: { browser: true, es2020: true }, extends: ["eslint:recommended"], ignorePatterns: ["dist", ".eslintrc.cjs"], rules: { "no-unused-vars": "warn", "no-empty": "warn", "react-hooks/exhaustive-deps": "warn", "react-hooks/set-state-in-effect": "warn" } } 
