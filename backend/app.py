27590c6a1016c0bfa98dc1b08f74f612ac0cf23e
simal24k <simal.mpgss@gmail.com>
2026-07-13 17:49:29 +0500

    Fix CORS for POST/PUT requests v3

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ def create_app():
     
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
         response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
         return response
